e2631b6a67a11bc44b0688a3230aa6e393e36c1b
Tim van Osch <timvanosch@pollex.nl>
2023-04-24 00:52:04 +0200

    Improve significantly by optimizing y checks

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,6 @@ function createColliderSystem() {
     // A list of entity IDs ordered by their bounding box min
     const listX = new Int32Array(10000);
     let listXSize = 0
-    const listY = new Int32Array(10000);
-    let listYSize = 0
 
     const sortList = () => {
         // Sort listX 
@@ -49,57 +47,27 @@ function createColliderSystem() {
                 listX[back] = listX[listXSize]
             }
         }
-        // Sort listY 
-        for (let i = 1; i < listYSize; i++) {
-            // Loop over all previous values
-            for (let back = i; back > 0; back--) {
-                const latter = CPosition.y[listY[back]] + CBoundingBox.y[listY[back]]
-                const former = CPosition.y[listY[back - 1]] + CBoundingBox.y[listY[back - 1]]
-                if (former < latter) break;
-                // Otherwise swap the two
-                listY[listYSize] = listY[back - 1]
-                listY[back - 1] = listY[back]
-                listY[back] = listY[listYSize]
-            }
-        }
     }
 
+    const addHit = (hitMap: Map<number, Set<number>>, a: number, b: number) => {
+        let set = hitMap.get(a) ?? new Set<number>();
+        set.add(b);
+        if (!hitMap.has(a)) {
+            hitMap.set(a, set);
+        }
+
+        set = hitMap.get(b) ?? new Set<number>();
+        set.add(a);
+        if (!hitMap.has(b)) {
+            hitMap.set(b, set);
+        }
+    }
     const calculateIntersections = () => {
         const activePool = new Set<number>()
         const hitMap = new Map<number, Set<number>>();
 
-        const addHit = (a: number, b: number) => {
-            let set = hitMap.get(a) ?? new Set<number>()
-            set.add(b)
-            hitMap.set(a, set)
-
-            set = hitMap.get(b) ?? new Set<number>()
-            set.add(a)
-            hitMap.set(b, set)
-        }
-        const removeHit = (a: number, b: number) => {
-            let set = hitMap.get(a)
-            if (set) {
-                set.delete(b)
-                if (set.size === 0) {
-                    hitMap.delete(a)
-                } else {
-                    hitMap.set(a, set)
-                }
-            }
-
-            set = hitMap.get(b)
-            if (set) {
-                set.delete(a)
-                if (set.size === 0) {
-                    hitMap.delete(b)
-                } else {
-                    hitMap.set(b, set)
-                }
-            }
-        }
-
         // Check X potentials
+        const overlaps = new Set<[number, number]>();
         for (let ix = 0; ix < listXSize; ix++) {
             const subject = listX[ix]
             for (const active of activePool) {
@@ -108,23 +76,19 @@ function createColliderSystem() {
                     activePool.delete(active);
                 } else {
                     // Otherwise there is an overlap and thus potential collision
-                    addHit(subject, active)
+                    overlaps.add([
+                        Math.min(active, subject),
+                        Math.max(active, subject),
+                    ])
                 }
             }
             activePool.add(subject);
         }
 
-        // Check Y potentials
-        activePool.clear()
-        for (let ix = 0; ix < listYSize; ix++) {
-            const subject = listY[ix]
-            const subjectHits = hitMap.get(subject)
-            if (subjectHits) {
-                for (const active of subjectHits) {
-                    if (CPosition.y[subject] + CBoundingBox.y[subject] > CPosition.y[active] + CBoundingBox.y[active] + CBoundingBox.h[active]) {
-                        removeHit(subject, active);
-                    }
-                }
+        for (const [a, b] of overlaps) {
+            if (!(CPosition.y[a] + CBoundingBox.y[a] + CBoundingBox.h[a] <= CPosition.y[b] + CBoundingBox.y[b] ||
+                CPosition.y[b] + CBoundingBox.y[b] + CBoundingBox.h[b] <= CPosition.y[a] + CBoundingBox.y[a])) {
+                addHit(hitMap, a, b);
             }
         }
 
@@ -136,36 +100,52 @@ function createColliderSystem() {
     const bbEnter = enterQuery(bb)
     const bbExit = exitQuery(bb)
 
-    return (world: IWorld) => {
+    let counter = 0;
+    let totalDuration = 0;
+    return function colliderSystem(world: IWorld) {
         for (const id of bbEnter(world)) {
-            listX[listXSize++] = id;
-            listY[listYSize++] = id;
+            listX[listXSize++] = id
         }
 
+        performance.mark('collider:start');
         sortList()
         const hitMap = calculateIntersections()
         for (const [id, hits] of hitMap.entries()) {
             hits.size > 0 && (CRenderRectangle.color[id] = 0xff0000);
         }
 
+        performance.mark('collider:end');
+        performance.measure('collider', 'collider:start', 'collider:end');
+
+        const measure = performance.getEntriesByName('collider')[0];
+        totalDuration += measure.duration;
+        counter++;
+
+        const averageDuration = totalDuration / counter;
+        console.log(`Average runtime: ${averageDuration} ms`);
+
+        performance.clearMarks('collider:start');
+        performance.clearMarks('collider:end');
+        performance.clearMeasures('collider');
+
+
 
         for (const id of bbExit(world)) {
             const idx = listX.indexOf(id)
             listX[idx] = listX[--listXSize];
-            const idy = listY.indexOf(id)
-            listY[idy] = listY[--listYSize];
         }
     }
 }
 
 function createRenderSystem(stage: Container) {
     const entityGraphicMap = new Map<number, Graphics>();
+    const lastColorCache = new Map<number, number>();
 
     const rectangles = defineQuery([CPosition, CRenderRectangle])
     const rectanglesEnter = enterQuery(rectangles);
     const rectanglesExit = exitQuery(rectangles);
 
-    return (world: IWorld) => {
+    return function renderSystem(world: IWorld) {
         for (const id of rectanglesEnter(world)) {
             const g = new Graphics();
             stage.addChild(g)
@@ -179,13 +159,17 @@ function createRenderSystem(stage: Container) {
             const c = CRenderRectangle.color[id]
 
             const g = entityGraphicMap.get(id)!
-            g.x = x
-            g.y = y
-            g.clear()
-            g.beginFill(c);
-            g.drawRect(0, 0, w, h)
-            g.endFill()
-
+            if (g.x != x || g.y != y) {
+                g.x = x
+                g.y = y
+            }
+            if (lastColorCache.get(id) != c) {
+                lastColorCache.set(id, c)
+                g.clear()
+                g.beginFill(c);
+                g.drawRect(0, 0, w, h)
+                g.endFill()
+            }
             CRenderRectangle.color[id] = 0x00ff00
 
         }
@@ -237,7 +221,7 @@ function createFollowMouseSystem() {
         mouseY = e.y
     })
 
-    return (world: IWorld) => {
+    return function followSystem(world: IWorld) {
         for (const id of query(world)) {
             CPosition.x[id] = mouseX
             CPosition.y[id] = mouseY
